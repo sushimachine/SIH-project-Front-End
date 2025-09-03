67b278a3330cc329280f3400a69b301fa6d9ca29
sushimachine <aryan456.rehla@gmail.com>
2025-09-03 15:21:31 +0000

    New Commit

--- DjangoJacks/src/App.jsx
+++ DjangoJacks/src/App.jsx
@@ -1,21 +1,17 @@
 import React from 'react';
 import './App.css'; // This file is now created to resolve the error.
 import ContactUsPage from './pages/contactUS';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
 
-// I've commented out the other page and the state for navigation
-// import AuthPage from './pages/signUpLogin';
-// import { useState } from 'react';
 
 function App() {
-  // The navigation state and logic have been temporarily removed.
-  // const [currentPage, setCurrentPage] = useState('auth');
-
+ 
   return (
     <div className="App">
-      {/* Now, we are directly rendering only the ContactUsPage component.
-        This allows you to see and work on it by itself.
-      */}
-      <ContactUsPage />
+      
+      {/* <ContactUsPage /> */}
+      <Login />
     </div>
   );
 }
